7b25fe940663a90c76f5f0484258f4c4a7dc16a0
goumix <nath.brault.pro@gmail.com>
2025-05-14 15:11:31 +0200

    add: frontend folder

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/marketplace',
-        permanent: true, // ou false si c'est temporaire
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
--- frontend/src/components/shared/footer.tsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  return (
-    <div className="flex flex-col items-center justify-center gap-4 p-4">
-      <h1 className="text-2xl font-bold">Omegaloops</h1>
-      <p className="text-sm">A dApp to tokenize samples</p>
-    </div>
-  );
-}
